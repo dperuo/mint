f8e0dc8ba6d20c65ef42f2c8cadc965914f2be17
dperuo <derek@dbpventures.com>
2014-05-25 08:37:46 -0400

    foundation

---
+++ config_prod.rb
@@ -0,0 +1,19 @@
+# Require any additional compass plugins here.
+add_import_path "bower_components/foundation/scss"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css/prod"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+
+# Select output style (can be overridden via the command line):
+output_style = :compressed
+
+# Enable relative paths to assets via compass helper functions
+# relative_assets = true
+
+# Disable debugging comments
+line_comments = false
+
