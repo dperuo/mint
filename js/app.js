4011f02cf23d17ac214f13455f3fda45204d137e
dperuo <derek@dbpventures.com>
2014-05-25 17:05:54 -0400

    start json

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+  
+  var $faq = $('#faq'),
+      $allFAQ = [];
+  
+  $.getJSON('js/faq.json', function(data){
+    
+    $(data.questions).each(function(i, value){
+      $allFAQ.push('<p>' + value.q + '<br>' + value.a + '</p>');
+    });
+    
+    $faq.html($allFAQ);
+        
+  });
+});
