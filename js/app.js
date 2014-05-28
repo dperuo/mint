c40666befb4b0b574f99a6f950a52d575fac3b92
dperuo <derek@dbpventures.com>
2014-05-27 22:08:30 -0400

    add styes to accordion

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $faq.append('<dl id="faqList" class="accordion" data-accordion></dl>')
   $.getJSON('js/faq.json', function(data){
     $(data.questions).each(function(i, value){
-      $('#faqList').append('<dd><a href="#panel' + (i+1) + '">' + value.q + '</a><div id="panel' + (i+1) + '" class="content">' + value.a + '</div></dd>')
+      $('#faqList').append('<dd><a href="#panel' + (i+1) + '"><h3 class="plus">' + value.q + '</h3 class="plus"></a><div id="panel' + (i+1) + '" class="content">' + value.a + '</div></dd>')
     })
   })
 
