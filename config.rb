f861f7f202cd6017c5fa566f323b399af3f21b7d
dperuo <derek@dbpventures.com>
2014-05-27 22:08:15 -0400

    use dev css file

--- config.rb
+++ config.rb
@@ -18,7 +18,7 @@ javascripts_dir = "js"
 # line_comments = false
 
 # Auto-compile production stylesheet
-on_stylesheet_saved do
-  `compass compile -c config_prod.rb --force`
-end
+# on_stylesheet_saved do
+#   `compass compile -c config_prod.rb --force`
+# end
 
